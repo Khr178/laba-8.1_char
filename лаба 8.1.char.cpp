d841316567b9179b2d99526cb392cf00bc11e714
Khrystyna <hrustunakopeika2005@gmail.com>
2022-11-26 17:43:36 +0200

    first master commit

---
+++ лаба 8.1.char.cpp
@@ -0,0 +1,29 @@
+﻿// Дано літерний рядок, який містить послідовність символів s0, …, sn, … .
+// 1. Вияснити, чи є серед цих символів всі букви, що входять в слово “while”.
+// 2. Замінити кожну групу букв “while” парою зірочок “ * *”.
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+bool Include(const char* s, const char* cs)
+{
+    bool r = true;
+    for (int i = 0; cs[i] != '\0'; i++)
+        r = r && (strchr(s, cs[i]));
+    
+    return r;
+}
+int main()
+{
+    char s[10] = "while";
+    char cs[] = "is";
+
+    if (Include(s, cs))
+        cout << "yes" << endl;
+    else
+        cout << "no" << endl;
+
+    return 0;
+}
---
+++ юніт тест 8.1.char.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../лаба 8.1.char/лаба 8.1.char.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace юніттест81char
+{
+	TEST_CLASS(юніттест81char)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char* s = "while";
+			char* cs = "is";
+			int expected = Include(s, cs);
+			Assert::AreEqual(expected, 0);
+		}
+	};
+}
